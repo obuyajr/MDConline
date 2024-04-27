d7481a372195cfcb9a6d1ba579bf430becc9a3a0
obuyajr <felix37brock@gmail.com>
2024-04-27 14:51:07 +0300

    loginpage

--- app/src/main/java/com/example/mdconline/loginpage.java
+++ app/src/main/java/com/example/mdconline/loginpage.java
@@ -3,6 +3,8 @@ package com.example.mdconline;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class loginpage extends AppCompatActivity {
+
+    TextInputEditText txtEmail, txtpassword;
+
+    Button signIn;
+    TextView signUp;
+
+    FirebaseAuth firebaseAuth =  FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +30,9 @@ public class loginpage extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loginpage);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
+        txtEmail = findViewById(R.id.email);
+        txtpassword =  findViewById((R.id.password));
+        
 
 
         }
